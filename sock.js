00e3895c2bdfd898fa0c86f8609bfba444a9bf1a
chinmai <me@chinmai.dev>
2021-02-12 23:23:59 -0800

    mayeb use the right source

--- sock.js
+++ sock.js
@@ -1,6 +1,6 @@
 // client-side
 const io = require("socket.io-client");
-const socket = io("https://api.example.com", {
+const socket = io("https://localhost:3000", {
   withCredentials: true,
   extraHeaders: {
     movesocket: "abcd",
